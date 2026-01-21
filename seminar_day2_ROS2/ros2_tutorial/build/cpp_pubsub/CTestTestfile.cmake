ef0022afeed18e3f684f2a939120cb10b9cd4fa1
inpyo <inpyoi1304@gmail.com>
2026-01-22 04:30:15 +0900

    [완료]

--- seminar_day2_ROS2/ros2_tutorial/build/cpp_pubsub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pyo/02_Work/04_Cpp/mcc_seminar/seminar_day2_ROS2/ros2_tutorial/src/cpp_pubsub
-# Build directory: /home/pyo/02_Work/04_Cpp/mcc_seminar/seminar_day2_ROS2/ros2_tutorial/build/cpp_pubsub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- seminar_day3_ROS2/tf2_practice_ws/build/tf2_broadcaster/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pyo/02_Work/04_Cpp/mcc_seminar/seminar_day3_ROS2/tf2_practice_ws/src/tf2_broadcaster
-# Build directory: /home/pyo/02_Work/04_Cpp/mcc_seminar/seminar_day3_ROS2/tf2_practice_ws/build/tf2_broadcaster
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
