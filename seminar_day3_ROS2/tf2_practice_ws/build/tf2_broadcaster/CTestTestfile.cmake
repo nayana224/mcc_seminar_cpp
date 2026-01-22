4d9344da1b160bd0a9d665a13432ee7c7c0ad0a7
inpyo <inpyoi1304@gmail.com>
2026-01-22 14:51:09 +0900

    [수정완료] 패키지 오류 잡기

---
+++ seminar_day3_ROS2/tf2_practice_ws/build/tf2_broadcaster/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pyo/mcc_seminar_cpp/seminar_day3_ROS2/tf2_practice_ws/src/tf2_broadcaster
+# Build directory: /home/pyo/mcc_seminar_cpp/seminar_day3_ROS2/tf2_practice_ws/build/tf2_broadcaster
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ seminar_day3_ROS2/urdf_practice_ws/build/edu_arm_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pyo/mcc_seminar_cpp/seminar_day3_ROS2/urdf_practice_ws/src/edu_arm_description
+# Build directory: /home/pyo/mcc_seminar_cpp/seminar_day3_ROS2/urdf_practice_ws/build/edu_arm_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
